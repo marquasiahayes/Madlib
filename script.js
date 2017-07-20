b8c8ff5bb533d5336467cea4e6c94613ec653c6b
marquasiahayes <hayes.marquasia@yahoo.com>
2017-07-20 13:20:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -10,16 +10,24 @@ questionsLeft = ' [' + questions + ' questions left]';
 var verb2 = prompt('Please type a verb that doesnt end in -ing ' + questionsLeft);
 questions -= 1;
 questionsLeft = ' [' + questions + ' questions left]';
-var adjective = prompt('Please type another adjective' + questionsLeft);
+var adjective = prompt('Please type a body part' + questionsLeft);
 questions -= 1;
-var adjective3 = prompt('Please type another adjective' + questionsLeft);
+var adjective3 = prompt('Please type an adjective' + questionsLeft);
 questions -= 1;
 var adverb2 = prompt('Just one more adverb' + questionsLeft);
 questions -= 1;
 var newname = prompt('Please type a your name' + questionsLeft);
 questions -= 1;
 alert('All done. Ready for the message?');
-var sentence = "<h2>Dear " + name
-sentence += ' I have been ' + verb
-sentence += ' about you all day long. I ' +  adverb sentence += 'the way you' + verb2 sentence += '. Your'+ adjective sentence += 'is very' + adjective3 sentence += 'I' +adverb2 sentence += 'you. Sincerely,' + newname sentence +='.</h2>';
-document.write(sentence);
+var sentence = "<p>Dear " + name;
+sentence += ' I have been ' + verb;
+sentence += ' about you all day long. I  ' +  adverb;
+sentence += ' the way you ' + verb2;
+sentence += '. Your'+ adjective;
+sentence += ' is very ' + adjective3;
+sentence += ' I ' +adverb2;
+sentence += ' you. Sincerely, ' + newname;
+sentence+= '</p>'
+// document.getElementById("demo").innerHTML=sentence;
+
+document.write(sentence.fontsize(89));
